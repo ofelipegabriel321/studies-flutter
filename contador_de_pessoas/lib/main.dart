98a4888f5f9686699123de379b3265ae073ab80a
Felipe Gabriel <ofelipegabriel321@gmail.com>
2020-02-25 17:47:20 -0300

    feat: add initial interface characteristics

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -1,13 +1,48 @@
 // import necessário para o runApp(app)
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(
-    // MaterialApp: widget necessário para que o App suporte widgets do Material Design.
-    MaterialApp(
-      title: "Contador de pessoas",
-      // Container: combina widgets comuns de painting, positioning e sizing.
-      home: Container(color: Colors.white,),
-    )
-  );
+      // MaterialApp: widget necessário para que o App suporte widgets do Material Design.
+      MaterialApp(
+          title: "Contador de pessoas",
+          // Column: widget que exibe seus children em uma matriz vertical.
+          home: Column(
+            // MainAxisAlignment: alinhamento do eixo principal.
+            mainAxisAlignment: MainAxisAlignment.center,
+            // <Widget>: É usado porque o children é uma lista de widgets.
+            children: <Widget>[
+              // Text: texto a ser exibido.
+              Text(
+                "Pessoas: 0",
+                style:
+                    // TextStyle: classe que configura do estilo do texto.
+                    // Colors: classe com a paleta de cores do Material Design.
+                    // FontWeight: classe da espessura da fonte.
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              // Row: widget que exibe seus filhos em uma matriz horizontal.
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // FlatButton: classe que define um botão
+                  FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+              Text(
+                "Pode Entrar!",
+                // FontStyle: classe da inclinação do texto.
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0),
+              )
+            ],
+          )));
 }
